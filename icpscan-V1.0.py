48138551fabaf82a1b0726108834910a2bf2cc0f
本间白猫 <139044047+honmashironeko@users.noreply.github.com>
2024-03-14 16:44:16 +0800

    Add files via upload

--- icpscan-V1.0.py
+++ icpscan-V1.0.py
@@ -19,7 +19,6 @@ import re
 import os
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-proxy_address = None
 # 正则匹配类
 FOFA_LINK_PATTERN = re.compile(r'<a href="([^"]*)" target="_blank">')
 
@@ -58,21 +57,20 @@ def base64_encode(ip):
     return base64.b64encode(f'is_domain=true && ip="{ip}"'.encode('utf-8')).decode('utf-8')
 
 # FOFA异步查询
-sem = asyncio.Semaphore(10)
-async def fofa_async(b64, ip, proxyip, retry_count=3):
+sem = asyncio.Semaphore(8)
+async def fofa_async(b64, ip, retry_count=3):
     async with sem:
         url = f"https://fofa.info/result?qbase64={b64}"
         for _ in range(retry_count):
             try:
-                proxies = f"http://{proxyip}" if proxyip else None
                 async with aiohttp.ClientSession() as session:
-                    async with session.get(url, headers=HEADERSF, timeout=10, verify_ssl=False, proxy=proxies) as response:
+                    async with session.get(url, headers=HEADERSF, timeout=10, verify_ssl=False) as response:
                         if response.status == 200:
                             text = await response.text()
                             if "资源访问每天限制" in text:
                                 print("当前IP已达到查询上限，请更换IP")
                                 return "stop"
-                            links = extract_links(text)
+                            links = extract_links(await response.text())
                             results = extract_domains(links)
                             print(f"FOFA查询完毕 IP: {ip}")
                             return results, ip
@@ -80,20 +78,17 @@ async def fofa_async(b64, ip, proxyip, retry_count=3):
                             print(f"FOFA请求失败！错误码: {response.status} - {ip}")
             except aiohttp.ClientError as e:
                 print(f"FOFA请求失败: {e} - {ip}")
+
         print(f"多次尝试后无法成功获取结果 - {ip}")
         return [], ip
-
-def get_icp_response(domain, headers, data, proxyip):
+    
+def get_icp_respons(domain, headers, data):
     url = "https://hlwicpfwc.miit.gov.cn/icpproject_query/api/icpAbbreviateInfo/queryByCondition"
-    proxies = {
-        'http': f"http://{proxyip}",
-        'https': f"http://{proxyip}"
-    }
-    response = requests.post(url, data=data, headers=headers, proxies=proxies, verify=False)
+    response = requests.post(url, data=data, headers=headers,  verify=False)
     return response
 
 # 查询备案
-def icp(domain, proxyip):
+def icp(domain):
     with open('token.txt', 'r') as file:
         lines = file.readlines()
         sign = lines[0].strip() 
@@ -124,7 +119,7 @@ def icp(domain, proxyip):
     data = '{"pageNum":"","pageSize":"","unitName":"'+ domain +'","serviceType":1}'
 
     requests.packages.urllib3.disable_warnings()
-    response = get_icp_response(domain, headers, data, proxyip)
+    response = get_icp_respons(domain, headers, data)
     if "过期" in response.text:
         token_get()
         with open('token.txt', 'r') as file:
@@ -135,7 +130,7 @@ def icp(domain, proxyip):
         headers["token"] = token
         headers["uuid"] = uuid
         headers["sign"] = sign
-        response = get_icp_response(domain, headers, data)
+        response = get_icp_respons(domain, headers, data)
 
     pattern = r'"unitName":"(.*?)"'
     match = re.search(pattern, response.text)
@@ -180,7 +175,7 @@ def token_get():
 
 
 # 爱站查询权重
-async def get_aizhan_rank(domaindj, proxyip):
+def get_aizhan_rank(domaindj):
     pcrank = "0"
     prrank = "0"
     url1 = f"https://rank.aizhan.com/{domaindj}/"
@@ -188,95 +183,77 @@ async def get_aizhan_rank(domaindj, proxyip):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
     }
+    try:
+        response1 = requests.get(url1, headers=headers, timeout=10)
+        if response1.status_code == 200:
+            pattern = r'id="br_pc_br"><img src="//statics\.aizhan\.com/images/br/(\d+)\.png"'
+            result1 = re.search(pattern, response1.text)
+            if result1:
+                pcrank = result1.group(1)
 
-
-    async with sem:
-        async with aiohttp.ClientSession() as session:
-            proxies = f"http://{proxyip}" if proxyip else None
-            async with session.get(url1, headers=headers, timeout=10, verify_ssl=False, proxy=proxies) as response1:
-                if response1.status == 200:
-                    pattern = r'id="br_pc_br"><img src="//statics\.aizhan\.com/images/br/(\d+)\.png"'
-                    result1 = re.search(pattern, await response1.text())
-                    if result1:
-                        pcrank = result1.group(1)
-
-            async with session.get(url2, headers=headers, timeout=10, verify_ssl=False, proxy=proxies) as response2:
-                if response2.status == 200:
-                    pattern = r'<img src="//statics.aizhan.com/images/pr/(\d+).png"'
-                    result2 = re.search(pattern, await response2.text())
-                    if result2:
-                        prrank = result2.group(1)
-
+        response2 = requests.get(url2, headers=headers, timeout=10)
+        if response2.status_code == 200:
+            pattern = r'<img src="//statics.aizhan.com/images/pr/(\d+).png"'
+            result2 = re.search(pattern, response2.text)
+            if result2:
+                prrank = result2.group(1)
+    except:
+        print("爱站请求失败，请检查网络")
     return pcrank, prrank
     
-async def df_to_excel(df):
-    df.to_excel('domain_icp_info.xlsx', index=False)
-
 # 主函数异步运行
-async def main_async(file_path, proxyip):
+async def main_async(file_path):
     domains = {}
     df = pd.DataFrame(columns=['IP地址', '域名', '备案信息', '百度权重', '谷歌权重'])
     listdomain = read_file(file_path)
     
     stop_flag = False
+    tasks = [] 
     for domain in listdomain:
-        tasks = []
         iplist, dmlist = ipdomain(domain)
-        
         if not stop_flag:
             for ip in iplist:
                 fofa_base64 = base64_encode(ip)
-                tasks.append(fofa_async(fofa_base64, ip, proxyip))
-                
-        for dm in dmlist:
-            domains.setdefault(' ', []).append(dm)
+                tasks.append(fofa_async(fofa_base64, ip))
 
-        results = await asyncio.gather(*tasks)
+    results = await asyncio.gather(*tasks)
 
-        for result in results:
-            if result == "stop":
-                stop_flag = True
-                print("检测到资源访问每天限制，停止所有FOFA函数，进入下一阶段")
-                break
+    for result in results:
+        if result == "stop":
+            stop_flag = True
+            print("检测到资源访问每天限制，停止所有FOFA函数，进入下一阶段")
+            break
 
-            extract_domains, ip = result
-            if ip in domains:
-                domains[ip].extend(extract_domains)
-            else:
-                domains[ip] = extract_domains
+        extracted_domains, ip = result
+        if ip in domains:
+            domains[ip].extend(extracted_domains)
+        else:
+            domains[ip] = extracted_domains
+
+    for dm in dmlist:
+        domains.setdefault(' ', []).append(dm)
 
     token_get()
     processed_domains = set()
     print("开始查询备案信息和域名权重")
-
-    tasks = []
-
     for ip, domain_list in domains.items():
         for domain in domain_list:
             domaindj = extract_domain(domain)
-
             if domaindj not in processed_domains:
-                beian = icp(domaindj, proxyip)
+                beian = icp(domaindj)
                 processed_domains.add(domaindj)
+                print(f"\n{domaindj} {beian}")
             else:
                 beian = "该子域名备案同主域名"
-
-            print(f"\n{domaindj} {beian}")
-
             pc_rank = ' '
             pr_rank = ' '
-
-            pcrank, prrank = await get_aizhan_rank(domaindj, proxyip)
-
+            pcrank, prrank = get_aizhan_rank(domaindj)
             if pcrank is not None:
                 pc_rank = pcrank
                 pr_rank = prrank
                 print(f"{domain} - 百度权重: {pc_rank} 谷歌权重: {pr_rank}")
             df = pd.concat([df, pd.DataFrame({'IP地址': [ip], '域名': [domain], '备案信息': [beian], '百度权重': [pc_rank], '谷歌权重': [pr_rank]})], ignore_index=True)
-            tasks.append(asyncio.create_task(df_to_excel(df)))
-
-    await asyncio.gather(*tasks)
-
+    df.to_excel('domain_icp_info.xlsx', index=False)
 
 github_repo = "https://api.github.com/repos/honmashironeko/icpscan/releases/latest"
 current_dir = os.getcwd()
@@ -349,19 +326,13 @@ def print_icpscan_banner():
     print("\t\t\t\t\t关注微信公众号:樱花庄的本间白猫")
     print("=" * 70)
     print("\t\tIcpScan开始执行")
-
 if __name__ == "__main__":
     check_for_updates()
     print_icpscan_banner()
     parser = argparse.ArgumentParser(description='ICPScan由本间白猫开发,旨在快速反查IP、域名归属')
     parser.add_argument('-f', dest='file_path', required=True, help='指定使用的路径文件 -f url.txt')
-    parser.add_argument('-p', dest='proxyip', help='指定代理地址 -p 127.0.0.1:8080 或者 -p user:pass@127.0.0.1:8080')
     args = parser.parse_args()
     file_path = args.file_path
-    proxyip = args.proxyip
-    start_time = time.time()
     loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(main_async(file_path, proxyip))
+    future = asyncio.ensure_future(main_async(file_path))
     loop.run_until_complete(future)
-    end_time = time.time()
-    print(f"处理完成，总耗时: {end_time - start_time:.2f} 秒")
